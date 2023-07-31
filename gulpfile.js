605d554e4dcca8174ef11b6f5e0a6c70f76498b7
viktoriastezko <viktoriastezhko@gmail.com>
2023-07-31 22:53:01 +0200

    feat: add data

--- gulpfile.js
+++ gulpfile.js
@@ -267,7 +267,12 @@ gulp.task('test', gulp.series( 'eslint', 'qunit' ))
 
 gulp.task('default', gulp.series(gulp.parallel('js', 'css', 'plugins'), 'test'))
 
-gulp.task('build', gulp.parallel('js', 'css', 'plugins'))
+gulp.task('html', () => {
+    return gulp.src('./index.html')
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', gulp.parallel('js', 'css', 'html', 'plugins'))
 
 gulp.task('package', gulp.series(() =>
 
